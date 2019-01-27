a70addeb57487984e041e8218943d019dcdb4d76
Fermenting <realferalxanderyell@gmail.com>
2019-01-27 02:55:46 -0800

    gifs added

--- src/App.js
+++ src/App.js
@@ -14,8 +14,9 @@ class App extends Component {
   state = {
     friends,
     markedArray : [1, 2, 3],
+    resultMessage: "click on any character to begin",
     score : 0,
-    // topScore: 0
+    topScore: 0
   };
 
   markIt = id => {
@@ -24,70 +25,43 @@ class App extends Component {
       //compare the id of the clicked card to the values in markedArray. 
       for (var i = 0; i<this.markedArray.length; i++){
       
-      //you chose a card that you chose already
+      //you chose a card that you chose already - GAME OVER
         if (this.markedArray[i] === id) {
-        this.setState({markedArray : [], score : 0 })
-        console.log(this.state)        
+          //reset the marked array to an empty array, set score to 0
+        this.setState({markedArray : [], score : 0, resultMessage: "You guessed wrong that time."})
+        //Shuffle the cards and re-render
 
+        //otherwise, you must have chosen a new character
       } else {
         
         this.setState({
+        //store the id of the character you chose in the marked array
           markedArray: this.markedArray.push(id), 
-          score: this.state.score + 1})
-        console.log(this.state)        
+          //increment your score
+          score: this.state.score + 1,
+          //display positive message
+          resultMessage: "You got it right!"
+        
+        })
+        //increment top score if necessary
+          if (this.state.score > this.state.topScore) {
+          this.setState({
+            topScore: this.state.score
+          })
+        }
+
+        //Shuffle the cards and re-render
       }
     }
   };
 
  
-  
-  
-      //If not in array:
-      
-      //
-
-
-
-    //What we need here is a way to track clicks and to re-render the cards in a random order. Here are the steps
-      //create & display a score as well as a top score
-
-        //Increment score by 1
-        //if current score is greater than top score, set top score equal to current score
-      //if object clicked was already true, game is over, and restarts.
-        //round score is set to 0
-        //all friend objecst values for 'marked' are set to 'false'
-        //friend cards are re-rendered.
-
-      // var marked = false
-    
-
-    
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    //We include only friends with an id property not equal to the id being received into this method. 
-    // const friends = this.state.friends.filter(friend => friend.id !== id);
-
-    // let notMarked = this.state.friends
-
-    // notMarked = this.state.friends.filter(friend => friend.id !== id);
-    
-    // console.log(notMarked)
-    // // Set this.state.friends equal to the new friends array
-    // //when we update our component's state by removing one of the friend objects, our component re-renders itself.
-    // this.setState({ friends });
-  
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  //whenever we map over a list of data and return JSX, 
-  //React wants us to give each element a unique key prop. React uses this value internally to help it efficiently render 
-  //and re-render components from arrays of data.
-
-  //the key prop is unusual because it's used by React but 
-  //isn't actually available for us to use inside of the component we pass it to. We pass the friend id in as a separate prop because we'll need it inside of the FriendCard component.
   render() {
     return (
       <Wrapper>
         <Title>Memory Game!</Title>
-        <Score>Memorized: {this.state.score}  ---  Best Ever: 12</Score>
+        <Score>{this.state.resultMessage}</Score>
+        <Score>Memorized: {this.state.score}  ---  Best Ever: {this.state.topScore}</Score>
         {this.state.friends.map(friend => (
           <FriendCard
             markIt={this.markIt}
