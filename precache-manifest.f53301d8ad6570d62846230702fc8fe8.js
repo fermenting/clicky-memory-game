66ab164652a93b51008307e854244344c0e766a4
fermenting <realferalxanderyell@gmail.com>
2020-01-29 19:45:49 -0800

    Updates

---
+++ precache-manifest.f53301d8ad6570d62846230702fc8fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65675be12e205bcb395038671c568c65",
+    "url": "/clicky-memory-game/index.html"
+  },
+  {
+    "revision": "506d6678dec01fbad270",
+    "url": "/clicky-memory-game/static/css/main.50d0c8e7.chunk.css"
+  },
+  {
+    "revision": "cedad24f1e8d2304f7d1",
+    "url": "/clicky-memory-game/static/js/2.c84f355e.chunk.js"
+  },
+  {
+    "revision": "b846902a03bddff09b754d87bdd08a3c",
+    "url": "/clicky-memory-game/static/js/2.c84f355e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "506d6678dec01fbad270",
+    "url": "/clicky-memory-game/static/js/main.1aa4d9f8.chunk.js"
+  },
+  {
+    "revision": "b091f65a9acac49b39d1",
+    "url": "/clicky-memory-game/static/js/runtime-main.a9aa49fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-memory-game/precache-manifest.d7cb4df771498fe30cc656c4e1647b4e.js"
+  "/clicky-memory-game/precache-manifest.f53301d8ad6570d62846230702fc8fe8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clicky-memory-game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-memory-game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
